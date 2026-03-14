34678d9bc703aafcb6d9452b89acb498cb0c418f
jmpark0611-art <jmpark0611@gmail.com>
2026-03-14 14:34:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 
+self.addEventListener('push', (event) => {
+    const options = {
+        body: '국방부 교육 수강신청 기간입니다. 잊지 말고 신청하세요!',
+        icon: 'https://cdn-icons-png.flaticon.com/512/869/869869.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/869/869869.png'
+    };
+    event.waitUntil(self.registration.showNotification('ROKA EDU-PASS', options));
+});
